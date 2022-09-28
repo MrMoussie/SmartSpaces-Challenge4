87d056b544351bccb7d66f3224b3a0ab1e44a04c
karinhiovic <100695296+karinhiovic@users.noreply.github.com>
2022-09-28 12:49:40 +0200

    Blinking LEDS

--- Buzzer_draft/Buzzer_draft.ino
+++ Buzzer_draft/Buzzer_draft.ino
@@ -1,6 +1,6 @@
 const int BUZZER = 33;
-const int GREEN_LED = 0; // TODO put in right PIN
-const int RED_LED = 1; // TODO put in right PIN
+const int GREEN_LED = 25; // TODO put in right PIN
+const int RED_LED = 26; // TODO put in right PIN
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,13 +21,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // TURN GREEN LED ON
+  // TURN GREEN AND LED ON
+  digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, HIGH);
 
   // BUZZING
   tone(BUZZER, 1000); //1kHz
   Serial.print("Buzzzzin");
   delay(1000); //1 sec
+
+  // TURN GREEN AND LED ON
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+
   tone(BUZZER, 100);
   Serial.print("Buzzzzin");
   delay(2000);
